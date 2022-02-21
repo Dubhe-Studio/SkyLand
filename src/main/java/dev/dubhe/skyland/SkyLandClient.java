93e90a93cdfd9770afb5b72e648bd6cbb2d8c2ff
Gu_ZT <administration@ztxy666.cn>
2022-02-21 20:35:05 +0800

    qwq

---
+++ src/main/java/dev/dubhe/skyland/SkyLandClient.java
@@ -0,0 +1,11 @@
+package dev.dubhe.skyland;
+
+import net.fabricmc.api.ClientModInitializer;
+
+
+public class SkyLandClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
