af0d059578b2d4510679f2c12c155bad2c97a6d1
Cjsah <2684117397@qq.com>
2022-03-20 22:44:03 +0800

    1.1.2

--- src/main/java/dev/dubhe/skyland/SkyLand.java
+++ src/main/java/dev/dubhe/skyland/SkyLand.java
@@ -5,6 +5,7 @@ import net.minecraft.world.GameRules;
 
 public class SkyLand implements ModInitializer {
     public static final GameRules.Key<GameRules.BooleanRule> LC = GameRules.register("qnmdLC", GameRules.Category.MOBS, GameRules.BooleanRule.create(false));
+    public static final GameRules.Key<GameRules.BooleanRule> CHIEFTAIN = GameRules.register("chieftainMode", GameRules.Category.MOBS, GameRules.BooleanRule.create(false));
 
     @Override
     public void onInitialize() {
---
+++ src/main/java/dev/dubhe/skyland/mixin/WanderingTraderManagerMixin.java
@@ -0,0 +1,18 @@
+package dev.dubhe.skyland.mixin;
+
+import dev.dubhe.skyland.SkyLand;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.WanderingTraderManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Random;
+
+@Mixin(WanderingTraderManager.class)
+public class WanderingTraderManagerMixin {
+    @Redirect(method = "trySpawn", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I"))
+    private int spawnX10(Random random, int value, ServerWorld world) {
+        return world.getGameRules().getBoolean(SkyLand.CHIEFTAIN) ? 0 : random.nextInt(value);
+    }
+}
