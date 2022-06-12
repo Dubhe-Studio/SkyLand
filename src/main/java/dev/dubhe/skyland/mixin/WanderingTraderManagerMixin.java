c8699b612bbf2f0f2b89716f02f032ea8afc3725
Gu_ZT <administration@ztxy666.cn>
2022-06-12 23:40:33 +0800

    update

--- src/main/java/dev/dubhe/skyland/mixin/WanderingTraderManagerMixin.java
+++ src/main/java/dev/dubhe/skyland/mixin/WanderingTraderManagerMixin.java
@@ -7,11 +7,11 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.Random;
+import net.minecraft.util.math.random.Random;
 
 @Mixin(WanderingTraderManager.class)
 public class WanderingTraderManagerMixin {
-    @Redirect(method = "trySpawn", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I"))
+    @Redirect(method = "trySpawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/random/Random;nextInt(I)I"))
     private int spawnX10(Random random, int value, ServerWorld world) {
         return world.getGameRules().getBoolean(SkyLand.CHIEFTAIN) ? 0 : random.nextInt(value);
     }
